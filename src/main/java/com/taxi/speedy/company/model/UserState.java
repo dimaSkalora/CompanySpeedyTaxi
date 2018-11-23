a68115649cb8cc1ee64fbd1805256a7818e86971
daredevil <timon2@ukr.net>
2018-11-23 12:09:35 +0200

    model - add class UserState, VehicleState

---
+++ src/main/java/com/taxi/speedy/company/model/UserState.java
@@ -0,0 +1,30 @@
+package com.taxi.speedy.company.model;
+
+public class UserState extends AbstractBaseEntity{
+    private String nameUS;
+
+    public UserState() {
+    }
+
+    public UserState(Integer id, String nameUS) {
+        super(id);
+        this.nameUS = nameUS;
+    }
+
+    public String getNameUS() {
+        return nameUS;
+    }
+
+    public void setNameUS(String nameUS) {
+        this.nameUS = nameUS;
+    }
+
+    @Override
+    public String toString() {
+        return "UserState{" +
+                "nameUS='" + nameUS + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/taxi/speedy/company/model/VehicleState.java
@@ -0,0 +1,29 @@
+package com.taxi.speedy.company.model;
+
+public class VehicleState extends AbstractBaseEntity {
+    private String nameVS;
+
+    public VehicleState() {
+    }
+
+    public VehicleState(Integer id, String nameVS) {
+        super(id);
+        this.nameVS = nameVS;
+    }
+
+    public String getNameVS() {
+        return nameVS;
+    }
+
+    public void setNameVS(String nameVS) {
+        this.nameVS = nameVS;
+    }
+
+    @Override
+    public String toString() {
+        return "VehicleState{" +
+                "nameVS='" + nameVS + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
