83b10d9ee13636187f86564cb333d2c512b420f1
KushchikovDA <timon2@ukr.net>
2020-07-08 14:24:34 +0300

    com.taxi.speedy.company.repository - VehicleRepository fix

--- src/main/java/com/taxi/speedy/company/repository/VehicleRepository.java
+++ src/main/java/com/taxi/speedy/company/repository/VehicleRepository.java
@@ -4,14 +4,13 @@ import com.taxi.speedy.company.model.Vehicle;
 
 import java.util.List;
 
-public interface VehicleRepositiry {
+public interface VehicleRepository {
     Vehicle save(Vehicle vehicle);
     // false if not found
     boolean delete(int id);
     // null if not found
     Vehicle get(int id);
     List<Vehicle> getAll();
-    // null if not found
-    Vehicle getByNameCar(String nameCar);
-    Vehicle getByVehicleNumber(String vehicleNumber);
+    List<Vehicle> getByNameCar(String nameCar);
+    List<Vehicle> getByVehicleNumber(String vehicleNumber);
 }
