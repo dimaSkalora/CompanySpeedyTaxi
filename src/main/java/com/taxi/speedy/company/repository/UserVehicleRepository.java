61d05aa083e48d807311328f1ce2ec82b318969b
KushchikovDA <timon2@ukr.net>
2020-07-13 10:24:52 +0300

    com.taxi.speedy.company.repository.jdbc - UserVehicleRepository fix

--- src/main/java/com/taxi/speedy/company/repository/UserVehicleRepository.java
+++ src/main/java/com/taxi/speedy/company/repository/UserVehicleRepository.java
@@ -6,14 +6,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface UserVehicleRepository {
-    UserVehicle save(UserVehicle userVehicle, int idUser);
-    boolean delete(int id, int idUser);
-    UserVehicle getByUser(int id, int idUser);
-    UserVehicle getByVehicle(int id, int idVehicle);
+    UserVehicle save(UserVehicle userVehicle);
+    boolean delete(int id);
+    UserVehicle get(int id);
+    List<UserVehicle> getAll();
     List<UserVehicle> getAllByUser(int idUser);
     List<UserVehicle> getAllByVehicle(int idVehicle);
     List<UserVehicle> getByIsCurrentUserMachine(int isCurrentUserMachine);
     // ORDERED date desc
-    List<UserVehicle> getBetween(LocalDate startDate, LocalDate endDate, int userId);
     List<UserVehicle> getBetween(LocalDate startDate, LocalDate endDate);
 }
