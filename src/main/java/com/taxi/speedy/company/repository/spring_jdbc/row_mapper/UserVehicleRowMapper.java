64534cd1289c6bbe8e85601e4bb7aaef7b288b63
KushchikovDA <timon2@ukr.net>
2020-07-14 10:09:06 +0300

    com.taxi.speedy.company.repository.spring_jdbc.row_mapper - UserVehicleRowMapper fix

--- src/main/java/com/taxi/speedy/company/repository/spring_jdbc/row_mapper/UserVehicleRowMapper.java
+++ src/main/java/com/taxi/speedy/company/repository/spring_jdbc/row_mapper/UserVehicleRowMapper.java
@@ -18,15 +18,34 @@ public class UserVehicleRowMapper implements RowMapper<UserVehicle> {
     public UserVehicle mapRow(ResultSet resultSet, int i) throws SQLException {
         //Методы ResultSet.getXXX предоставляют доступ к значениям в колонках в текущей строке.
         // В пределах одной строки значения могут быть считаны в любом порядке
-        UserVehicle userVehicle = new UserVehicle();
+        User user = new User();
+        user.setId(resultSet.getInt("u_id"));
+        user.setName(resultSet.getString("u_name"));
+        user.setEmail(resultSet.getString("u_email"));
+        user.setPassword(resultSet.getString("u_password"));
+        user.setPhone(resultSet.getString("u_phone"));
+        user.setAddress(resultSet.getString("u_address"));
+        user.setRegistered(resultSet.getTimestamp("u_registered"));
+        user.setEnabled(resultSet.getBoolean("u_enabled"));
 
-        userVehicle.setId(resultSet.getInt(1));                                     //userVehicle.setId(resultSet.getInt("id"));
-        userVehicle.setStartDate(resultSet.getTimestamp(2).toLocalDateTime());      //userVehicle.setStartDate(resultSet.getTimestamp("start_date").toLocalDateTime());
+        Vehicle vehicle = new Vehicle();
+        vehicle.setId(resultSet.getInt("v_id"));
+        vehicle.setNameCar(resultSet.getString("v_name_car"));
+        vehicle.setVehicleNumber(resultSet.getString("v_vehicle_number"));
+        vehicle.setYearIssue(resultSet.getInt("v_year_issue"));
+        vehicle.setCategory(resultSet.getString("v_category"));
+        vehicle.setColor(resultSet.getString("v_color"));
+        vehicle.setFuelConsumption(resultSet.getInt("v_fuel_consumption"));
+
+        UserVehicle userVehicle = new UserVehicle();
+        userVehicle.setId(resultSet.getInt("uv_id"));                                     //userVehicle.setId(resultSet.getInt(1));
+        userVehicle.setStartDate(resultSet.getTimestamp("uv_start_date").toLocalDateTime());      //userVehicle.setStartDate(resultSet.getTimestamp("start_date").toLocalDateTime());
         //userVehicle.setStartDate(resultSet.getObject(2,LocalDateTime.class));                 //userVehicle.setStartDate(resultSet.getObject(2,LocalDateTime.class));
-        userVehicle.setEndDate(resultSet.getTimestamp(3).toLocalDateTime());
-        userVehicle.setIdUser((User) resultSet.getObject(4));                       //userVehicle.setIdUser((User) resultSet.getObject("id_user"));
-        userVehicle.setIdVehicle((Vehicle) resultSet.getObject(5));
-        userVehicle.setIsCurrentUserMachine(resultSet.getInt(6));
+        if (resultSet.getTimestamp("uv_end_date") != null)
+            userVehicle.setEndDate(resultSet.getTimestamp("uv_end_date").toLocalDateTime());
+        userVehicle.setIdUser(user);                       //userVehicle.setIdUser((User) resultSet.getObject("id_user"));
+        userVehicle.setIdVehicle(vehicle);
+        userVehicle.setIsCurrentUserMachine(resultSet.getInt("v_fuel_consumption"));
 
         return userVehicle;
     }
