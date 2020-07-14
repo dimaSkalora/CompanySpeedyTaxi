188598f3319daaa3f781871945cab8b79f8f9f62
KushchikovDA <timon2@ukr.net>
2020-07-14 13:39:34 +0300

    com.taxi.speedy.company.web.user_vehicle - add AbstractUserVehicleController

---
+++ src/main/java/com/taxi/speedy/company/web/user_vehicle/AbstractUserVehicleController.java
@@ -0,0 +1,72 @@
+package com.taxi.speedy.company.web.user_vehicle;
+
+import com.taxi.speedy.company.model.UserVehicle;
+import com.taxi.speedy.company.service.UserVehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public abstract class AbstractUserVehicleController {
+    private final Logger log = LoggerFactory.getLogger(AbstractUserVehicleController.class);
+
+    @Autowired
+    private UserVehicleService userVehicleService;
+
+    public UserVehicle create(UserVehicle userVehicle){
+        log.info("create {}",userVehicle);
+        if (!userVehicle.isNew())
+            throw new IllegalArgumentException(userVehicle+" должен быть новым (id == null)");
+        return userVehicleService.create(userVehicle);
+    }
+
+    public void update(UserVehicle userVehicle){
+        log.info("update {}",userVehicle);
+        if (userVehicle.isNew())
+            throw new IllegalArgumentException(userVehicle+" должен быть не новым (id != null)");
+        userVehicleService.update(userVehicle);
+    }
+
+    public boolean delete(int id){
+        log.info("delete {}",id);
+        return userVehicleService.delete(id);
+    }
+
+    public UserVehicle get(int id){
+        log.info("get {}",id);
+        return userVehicleService.get(id);
+    }
+
+    public List<UserVehicle> getAll(){
+        log.info("getAll");
+        return userVehicleService.getAll();
+    }
+
+    public List<UserVehicle> getAllByUser(int idUser){
+        log.info("getAllByUser idUser {}",idUser);
+        return userVehicleService.getAllByUser(idUser);
+    }
+
+    public List<UserVehicle> getAllByVehicle(int idVehicle){
+        log.info("getAllByVehicle idVehicle {}",idVehicle);
+        return userVehicleService.getAllByVehicle(idVehicle);
+    }
+
+    public List<UserVehicle> getByIsCurrentUserMachine(int isCurrentUserMachine){
+        log.info("getByIsCurrentUserMachine isCurrentUserMachine {}",isCurrentUserMachine);
+        return userVehicleService.getByIsCurrentUserMachine(isCurrentUserMachine);
+    }
+
+    public List<UserVehicle> getStartDateBetween(LocalDate startDate, LocalDate endDate){
+        log.info("getStartDateBetween startDate {}, endDate {}",startDate, endDate);
+        return userVehicleService.getStartDateBetween(startDate, endDate);
+    }
+
+    public List<UserVehicle> getEndDateBetween(LocalDate startDate, LocalDate endDate){
+        log.info("getEndDateBetween startDate {}, endDate {}",startDate, endDate);
+        return userVehicleService.getEndDateBetween(startDate, endDate);
+    }
+
+}
