67b73ca4b855e48389cb73a1fe8caa8f740b388a
KushchikovDA <timon2@ukr.net>
2020-08-12 15:37:49 +0300

    com.taxi.speedy.company.repository.spring_jdbc - JdbcAccessToRouteRepositoryImpl fix

--- src/main/java/com/taxi/speedy/company/repository/spring_jdbc/JdbcAccessToRouteRepositoryImpl.java
+++ src/main/java/com/taxi/speedy/company/repository/spring_jdbc/JdbcAccessToRouteRepositoryImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -36,22 +37,23 @@ public class JdbcAccessToRouteRepositoryImpl implements AccessToRouteRepository 
     */
     private SimpleJdbcInsert jdbcInsert;
 
-    private final String sqlQuery ="select  atr.id as atr_id, atr.checks_date_time as atr_checksdatetime, atr.id_user_vehicles as atr_id_user_vehicles \n" +
+    private final String sqlQuery ="select  atr.id as atr_id, atr.checks_date_time as atr_checksdatetime, atr.id_user_vehicle as atr_id_user_vehicles \n" +
             " ,atr.id_user_state as atr_id_user_state, atr.id_vehicle_state as atr_id_vehicle_state, atr.is_access as atr_is_access \n" +
             " ,uv.id as uv_id, uv.start_date as uv_start_date, uv.end_date as uv_end_date, uv.id_user as uv_id_user \n" +
             " ,uv.id_vehicle as uv_id_vehicle, uv.is_current_user_machine as uv_is_current_user_machine \n" +
-            " ,us.id as us_id, us.name_us as us_name_us, vs.id as vs_id, vs.name_vs as vs_name_vs" +
+            " ,us.id as us_id, us.name_us as us_name_us, vs.id as vs_id, vs.name_vs as vs_name_vs \n" +
+            " ,u.id as u_id, u.name as u_name, u.email as u_email \n" +
             " ,u.password as u_password, u.phone as u_phone, u.address as u_address \n" +
-            " ,u.registered as u_registered, u.enabled as u_enabled" +
+            " ,u.registered as u_registered, u.enabled as u_enabled \n" +
             " ,v.id as v_id, v.name_car as v_name_car \n" +
-            " ,v.vehicle_number as v_vehicle_number, v.year_issue as v_year_issue, v.category as v_category " +
+            " ,v.vehicle_number as v_vehicle_number, v.year_issue as v_year_issue, v.category as v_category \n" +
             " ,v.color as v_color, v.fuel_consumption as v_fuel_consumption \n" +
             " from access_to_route atr " +
-            " left join user_vehicles uv on atr.id_user_vehicles = uv.id \n" +
+            " left join user_vehicles uv on atr.id_user_vehicle = uv.id \n" +
             " left join user_state us on atr.id_user_state = us.id \n" +
             " left join vehicle_state vs on atr.id_vehicle_state = vs.id \n" +
             " left join users u on uv.id_user = u.id \n" +
-            " left join vehicles v on uv.id_vehicle = v.id";
+            " left join vehicles v on uv.id_vehicle = v.id \n";
 
     @Autowired
     public JdbcAccessToRouteRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -66,6 +68,32 @@ public class JdbcAccessToRouteRepositoryImpl implements AccessToRouteRepository 
 
     @Override
     public AccessToRoute save(AccessToRoute accessToRoute) {
+        //Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                        .addValue("id",accessToRoute.getId())
+                        .addValue("checksDateTime",accessToRoute.getChecksDateTime())
+                        .addValue("idUserVehicle",accessToRoute.getIdUserVehicle().getId())
+                        .addValue("idUserState",accessToRoute.getIdUserState().getId())
+                        .addValue("idVehicleState",accessToRoute.getIdVehicleState().getId())
+                        .addValue("isAccess",accessToRoute.getIsAccess());
+
+          /*  BeanPropertySqlParameterSource - анализирует переданный ему объект и для каждого свойства
+            объекта создаёт параметр с именем свойства и его значением.
+         */
+        BeanPropertySqlParameterSource beanPropertySqlParameterSource = new BeanPropertySqlParameterSource(accessToRoute);
+
+        if (accessToRoute.isNew()){
+            //Выполните вставку, используя значения, переданные и возвращающие сгенерированный ключ.
+            //Number newKey = jdbcInsert.executeAndReturnKey(beanPropertySqlParameterSource);
+            Number newKey = jdbcInsert.executeAndReturnKey(mapSqlParameterSource);
+            accessToRoute.setId(newKey.intValue());
+        }else {
+            if(namedParameterJdbcTemplate.update("UPDATE access_to_route SET checks_date_time=:checksDateTime," +
+                    " id_user_vehicle=:idUserVehicle, id_user_state=:idUserState, " +
+                    " id_vehicle_state=:idVehicleState, is_access=:isAccess WHERE id=:id",mapSqlParameterSource) == 0){
+                return null;
+            }
+        }
 
         return accessToRoute;
     }
@@ -91,27 +119,67 @@ public class JdbcAccessToRouteRepositoryImpl implements AccessToRouteRepository 
 
     @Override
     public List<AccessToRoute> getAll() {
-        List<AccessToRoute> accessToRouteList = jdbcTemplate.query("SELECT * FROM access_to_route",ROW_MAPPER_ACCESS_TO_ROUTE);
+        //List<AccessToRoute> accessToRouteList = jdbcTemplate.query("SELECT * FROM access_to_route",ROW_MAPPER_ACCESS_TO_ROUTE);
+
+
+        List<AccessToRoute> accessToRouteList = jdbcTemplate.query(sqlQuery,new AccessToRouteRowMapper());
         return accessToRouteList;
     }
 
     @Override
     public List<AccessToRoute> getByUserVehicle(int idUserVehicle) {
-        return null;
+        //List<AccessToRoute> userVehicles = jdbcTemplate.query("SELECT * FROM user_vehicles WHERE idUserVehicle=?",ROW_MAPPER_ACCESS_TO_ROUTE,idUser);
+
+        String sqlByUserVehicle = sqlQuery + " where id_user_vehicle=:idUserVehicle";
+        //MapSqlParameterSource - Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("idUserVehicle", idUserVehicle);
+        List<AccessToRoute> accessToRouteList = namedParameterJdbcTemplate.query(sqlByUserVehicle
+                ,mapSqlParameterSource,new AccessToRouteRowMapper());
+
+        return accessToRouteList;
     }
 
     @Override
     public List<AccessToRoute> getByUserState(int idUserState) {
-        return null;
+        //Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
+     /*   MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("idUserState", idUserState);
+        List<AccessToRoute> accessToRouteList = namedParameterJdbcTemplate.query("SELECT * FROM access_to_route where id_user_state=:idUserState"
+                ,mapSqlParameterSource,new AccessToRouteRowMapper());*/
+        String sqlByUserState = sqlQuery + " where id_user_state=?";
+        List<AccessToRoute> accessToRouteList = jdbcTemplate.query(sqlByUserState,new AccessToRouteRowMapper(),idUserState);
+
+        return accessToRouteList;
     }
 
     @Override
     public List<AccessToRoute> getByVehicleState(int idVehicleState) {
-        return null;
+        //String sqlQuery = "SELECT * from access_to_route where id_vehicle_state=?";
+        //List<AccessToRoute> accessToRouteList = jdbcTemplate.query(sqlQuery,new AccessToRouteRowMapper(),idVehicleState);
+
+        String sqlByVehicleState = sqlQuery + " where id_vehicle_state=:idVehicleState";
+        //Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("idVehicleState", idVehicleState);
+        List<AccessToRoute> accessToRouteList = namedParameterJdbcTemplate.query(sqlByVehicleState
+                ,mapSqlParameterSource,new AccessToRouteRowMapper());
+
+        return accessToRouteList;
     }
 
     @Override
     public List<AccessToRoute> getByIsAccess(int isAccess) {
-        return null;
+     /*   String sqlQuery = "SELECT * from access_to_route where is_access=:isAccess";
+        //Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("isAccess", isAccess);
+        List<AccessToRoute> accessToRouteList = namedParameterJdbcTemplate.query(sqlQuery
+                ,mapSqlParameterSource,new AccessToRouteRowMapper());*/
+
+        String sqlByVehicleState = sqlQuery +" where is_access=?";
+        List<AccessToRoute> accessToRouteList = jdbcTemplate.query(sqlByVehicleState,new AccessToRouteRowMapper(),isAccess);
+
+        return accessToRouteList;
     }
 }
