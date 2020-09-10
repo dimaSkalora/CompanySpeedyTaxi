75ee4773a6f8f584839219785d2a36495190b116
Lenovo <timon2@ukr.net>
2020-09-10 20:27:14 +0300

    com.taxi.speedy.company.web.taxi_order_acceptance - add AbstractTaxiOrderAcceptanceController

---
+++ src/main/java/com/taxi/speedy/company/web/taxi_order_acceptance/AbstractTaxiOrderAcceptanceController.java
@@ -0,0 +1,71 @@
+package com.taxi.speedy.company.web.taxi_order_acceptance;
+
+import com.taxi.speedy.company.model.TaxiDispatcherOrder;
+import com.taxi.speedy.company.model.TaxiOrderAcceptance;
+import com.taxi.speedy.company.model.TaxiUserOrder;
+import com.taxi.speedy.company.service.TaxiDispatcherOrderService;
+import com.taxi.speedy.company.service.TaxiOrderAcceptanceService;
+import com.taxi.speedy.company.service.TaxiUserOrderService;
+import com.taxi.speedy.company.service.UserVehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.taxi.speedy.company.util.ValidationUtil.checkNew;
+import static com.taxi.speedy.company.util.ValidationUtil.checkNotNew;
+
+public abstract class AbstractTaxiOrderAcceptanceController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    TaxiOrderAcceptanceService taxiOrderAcceptanceService;
+    @Autowired
+    UserVehicleService userVehicleService;
+    @Autowired
+    TaxiDispatcherOrderService taxiDispatcherOrderService;
+    @Autowired
+    TaxiUserOrderService taxiUserOrderService;
+
+    public TaxiOrderAcceptance create(TaxiOrderAcceptance taxiOrderAcceptance){
+        checkNew(taxiOrderAcceptance);
+        log.info("create {}",taxiOrderAcceptance);
+
+        return taxiOrderAcceptanceService.create(taxiOrderAcceptance);
+    }
+
+    public void update(TaxiOrderAcceptance taxiOrderAcceptance){
+        checkNotNew(taxiOrderAcceptance);
+
+        log.info("update {}", taxiOrderAcceptance);
+        taxiOrderAcceptanceService.update(taxiOrderAcceptance);
+    }
+
+    public boolean delete(int id){
+        log.info("delete id ={}",id);
+        return taxiOrderAcceptanceService.delete(id);
+    }
+
+    public TaxiOrderAcceptance get(int id){
+        log.info("get id = {}",id);
+        return taxiOrderAcceptanceService.get(id);
+    }
+
+    public List<TaxiOrderAcceptance> getAll(){
+        log.info("getAll");
+        return taxiOrderAcceptanceService.getAll();
+    }
+
+    public List<TaxiOrderAcceptance> filterTaxiUserOrder(TaxiOrderAcceptance taxiOrderAcceptance){
+        log.info("filterTaxiUserOrder {}",taxiOrderAcceptance);
+        return taxiOrderAcceptanceService.filterTaxiUserOrder(taxiOrderAcceptance);
+    }
+
+    public  List<TaxiOrderAcceptance> filterTaxiUserOrderSqlCondition(TaxiOrderAcceptance taxiOrderAcceptance,String sqlCondition){
+        log.info("filterTaxiUserOrderSqlCondition taxiOrderAcceptance {}, sqlCondition {}",taxiOrderAcceptance,sqlCondition);
+        return taxiOrderAcceptanceService.filterTaxiUserOrder(taxiOrderAcceptance,sqlCondition);
+    }
+
+
+}
