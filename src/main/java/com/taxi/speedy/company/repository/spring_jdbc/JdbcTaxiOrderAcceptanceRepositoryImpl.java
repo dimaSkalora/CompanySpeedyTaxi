e8a0ca534979144b4cd52e9c5374a4d8c509e838
KushchikovDA <timon2@ukr.net>
2020-09-10 15:23:44 +0300

    com.taxi.speedy.company.repository.* - TaxiOrderAcceptanceRowMapper, JdbcTaxiOrderAcceptanceRepositoryImpl fix

--- src/main/java/com/taxi/speedy/company/repository/spring_jdbc/JdbcTaxiOrderAcceptanceRepositoryImpl.java
+++ src/main/java/com/taxi/speedy/company/repository/spring_jdbc/JdbcTaxiOrderAcceptanceRepositoryImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -38,16 +39,18 @@ public class JdbcTaxiOrderAcceptanceRepositoryImpl implements TaxiOrderAcceptanc
     private static BeanPropertyRowMapper<TaxiOrderAcceptance> ROW_MAPPER_TO_ACCEPTANCE = BeanPropertyRowMapper.newInstance(TaxiOrderAcceptance.class);
 
     private final String sqlQuery =" select  toa.id as toa_id, toa.id_user_vehicle as toa_id_user_vehicle, \n" +
-            " toa.id_taxi_dispatcher_order as toa_id_taxi_dispatcher_order, toa.id_taxi_user_order as toa.id_taxi_user_order,\n" +
+            " toa.id_taxi_dispatcher_order as toa_id_taxi_dispatcher_order, toa.id_taxi_user_order as toa_id_taxi_user_order,\n" +
             " toa.execution_status as toa_execution_status, toa.adoption_status as toa_adoption_status, \n " +
             " uv.id as uv_id, uv.start_date as uv_start_date, uv.end_date as uv_end_date, uv.id_user as uv_id_user, \n" +
             " uv.id_vehicle as uv_id_vehicle, uv.is_current_user_machine as uv_is_current_user_machine, \n" +
             " tdo.id as tdo_id, tdo.date_time_order as dto_date_time_order, tdo.id_taxi_dispatcher as tdo_id_td,\n" +
             " tdo.user_name as tdo_user_name, tdo.user_phone as tdo_user_phone,\n" +
+            " tdo.address_departure as tdo_address_departure, tdo.address_arrival as tdo_address_arrival,\n" +
+            " tdo.start_date as tdo_start_date, tdo.end_date as tdo_end_date, tdo.fulfilled as tdo_fulfilled,\n" +
             " tuo.id as tuo_id, tuo.date_time_order as tuo_date_time_order, tuo.id_user as tuo_id_user,\n"+
             " tuo.address_departure as tuo_address_departure, \n"+
             " tuo.address_arrival as tuo_address_arrival, tuo.start_date as tuo_start_date,\n"+
-            " tuo.end_date as tuo_end_date, tuo.fulfilled as tuo_fulfilled,\n"+
+            " tuo.end_date as tuo_end_date, tuo.fulfilled as tuo_fulfilled\n"+
             " from taxi_order_acceptance toa \n" +
             " left join user_vehicles uv on toa.id_user_vehicle = uv.id\n" +
             " left join taxi_dispatcher_orders tdo on toa.id_taxi_dispatcher_order = tdo.id\n" +
@@ -68,7 +71,31 @@ public class JdbcTaxiOrderAcceptanceRepositoryImpl implements TaxiOrderAcceptanc
     public TaxiOrderAcceptance save(TaxiOrderAcceptance taxiOrderAcceptance) {
         //Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
         MapSqlParameterSource parameterSource = new MapSqlParameterSource()
-                .addValue("id",taxiOrderAcceptance.getId());
+                .addValue("id",taxiOrderAcceptance.getId())
+                .addValue("idUserVehicle",taxiOrderAcceptance.getIdUserVehicle().getId());
+        if(taxiOrderAcceptance.getIdTaxiDispatcherOrder() != null)
+            parameterSource.addValue("idTaxiDispatcherOrder",taxiOrderAcceptance.getIdTaxiDispatcherOrder().getId());
+        if(taxiOrderAcceptance.getIdTaxiUserOrder() != null)
+            parameterSource.addValue("idTaxiUserOrder",taxiOrderAcceptance.getIdTaxiUserOrder().getId());
+        parameterSource.addValue("executionStatus",taxiOrderAcceptance.getExecutionStatus());
+        parameterSource.addValue("adoptionStatus",taxiOrderAcceptance.getAdoptionStatus());
+
+        /*  BeanPropertySqlParameterSource - анализирует переданный ему объект и для каждого свойства
+            объекта создаёт параметр с именем свойства и его значением.
+         */
+        BeanPropertySqlParameterSource propertySqlParameterSource = new BeanPropertySqlParameterSource(taxiOrderAcceptance);
+
+        if (taxiOrderAcceptance.isNew()){
+            Number newKey = simpleJdbcInsert.executeAndReturnKey(parameterSource);
+            //Number newKey = simpleJdbcInsert.executeAndReturnKey(propertySqlParameterSource);
+
+            taxiOrderAcceptance.setId(newKey.intValue());
+        }else if (namedParameterJdbcTemplate.update("update taxi_order_acceptance set id_user_vehicle=:idUserVehicle,\n" +
+                        " id_taxi_dispatcher_order=:idTaxiDispatcherOrder, id_taxi_user_order=:idTaxiUserOrder,\n" +
+                        " executionStatus=:executionStatus, adoptionStatus=:adoptionStatus\n" +
+                        " where id=:id", parameterSource) == 0){
+            return taxiOrderAcceptance;
+        }
 
         return taxiOrderAcceptance;
     }
@@ -92,7 +119,7 @@ public class JdbcTaxiOrderAcceptanceRepositoryImpl implements TaxiOrderAcceptanc
                 .addValue("id",id);
         List<TaxiOrderAcceptance> list = namedParameterJdbcTemplate.query(sqlQueryGet,parameterSource,new TaxiOrderAcceptanceRowMapper()); */
 
-        String sqlQueryGet = sqlQuery+" where id=?";
+        String sqlQueryGet = sqlQuery+" where toa.id=?";
         List<TaxiOrderAcceptance> list = jdbcTemplate.query(sqlQueryGet,new TaxiOrderAcceptanceRowMapper(),id);
 
         return DataAccessUtils.singleResult(list);//Возвращает один объект результата из данной коллекции.
@@ -110,6 +137,59 @@ public class JdbcTaxiOrderAcceptanceRepositoryImpl implements TaxiOrderAcceptanc
 
     @Override
     public List<TaxiOrderAcceptance> filterTaxiUserOrder(TaxiOrderAcceptance taxiOrderAcceptance, String sqlCondition) {
-        return null;
+        String sqlFilterTaxiUserOrder = sqlQuery;
+        List<TaxiOrderAcceptance> list;
+        int count = 0;
+
+        //Этот класс предназначен для передачи в простой Map значений параметров методам NamedParameterJdbcTemplate класса.
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        if(taxiOrderAcceptance.getId() != null)
+            parameterSource.addValue("id",taxiOrderAcceptance.getId());
+        if(taxiOrderAcceptance.getIdUserVehicle() != null)
+            parameterSource.addValue("idUserVehicle",taxiOrderAcceptance.getIdUserVehicle());
+        if(taxiOrderAcceptance.getIdTaxiDispatcherOrder() != null)
+            parameterSource.addValue("idTaxiDispatcherOrder",taxiOrderAcceptance.getIdTaxiDispatcherOrder());
+        if(taxiOrderAcceptance.getIdTaxiUserOrder() != null)
+            parameterSource.addValue("idTaxiUserOrder",taxiOrderAcceptance.getIdTaxiUserOrder());
+        if(taxiOrderAcceptance.getExecutionStatus() != null)
+            parameterSource.addValue("executionStatus",taxiOrderAcceptance.getExecutionStatus());
+        if(taxiOrderAcceptance.getAdoptionStatus() != null)
+            parameterSource.addValue("adoptionStatus",taxiOrderAcceptance.getAdoptionStatus());
+
+        for(var mapEntry: parameterSource.getValues().entrySet()){
+            String paramName = mapEntry.getKey();
+
+            if (count==0){
+                switch (paramName){
+                    case "id"                       ->   sqlFilterTaxiUserOrder = sqlFilterTaxiUserOrder+" where toa.id=:id\n";
+                    case "idUserVehicle"            ->   sqlFilterTaxiUserOrder += " where toa.id_user_vehicle=:idUserVehicle\n";
+                    case "idTaxiDispatcherOrder"    ->   sqlFilterTaxiUserOrder += " where toa.id_taxi_dispatcher_order=:idTaxiDispatcherOrder\n";
+                    case "idTaxiUserOrder"          ->   sqlFilterTaxiUserOrder += " where toa.id_taxi_user_order=:idTaxiUserOrder\n";
+                    case "executionStatus"          ->   sqlFilterTaxiUserOrder += " where toa.execution_status=:executionStatus\n";
+                    case "adoptionStatus"           ->   sqlFilterTaxiUserOrder += " where toa.adoption_status=:adoptionStatus\n";
+                }
+            }else {
+                switch (paramName){
+                    case "id"                       ->   sqlFilterTaxiUserOrder = sqlFilterTaxiUserOrder+" and toa.id=:id\n";
+                    case "idUserVehicle"            ->   sqlFilterTaxiUserOrder += " and toa.id_user_vehicle=:idUserVehicle\n";
+                    case "idTaxiDispatcherOrder"    ->   sqlFilterTaxiUserOrder += " and toa.id_taxi_dispatcher_order=:idTaxiDispatcherOrder\n";
+                    case "idTaxiUserOrder"          ->   sqlFilterTaxiUserOrder += " and toa.id_taxi_user_order=:idTaxiUserOrder\n";
+                    case "executionStatus"          ->   sqlFilterTaxiUserOrder += " and toa.execution_status=:executionStatus\n";
+                    case "adoptionStatus"           ->   sqlFilterTaxiUserOrder += " and toa.adoption_status=:adoptionStatus\n";
+                }
+            }
+            count++;
+        }
+
+        if ((sqlCondition != null) && (!sqlCondition.equals(""))){
+            if (parameterSource.getParameterNames().length > 0)
+                sqlFilterTaxiUserOrder += "and ( "+sqlCondition+" )";
+            else
+                sqlFilterTaxiUserOrder += " where ("+sqlCondition+" )";
+        }
+        
+        list = namedParameterJdbcTemplate.query(sqlFilterTaxiUserOrder,parameterSource,new TaxiOrderAcceptanceRowMapper());
+        
+        return list;
     }
 }
--- src/main/java/com/taxi/speedy/company/repository/spring_jdbc/row_mapper/TaxiOrderAcceptanceRowMapper.java
+++ src/main/java/com/taxi/speedy/company/repository/spring_jdbc/row_mapper/TaxiOrderAcceptanceRowMapper.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 //RowMapper <T> - используется JdbcTemplate для отображения строк ResultSet для каждой строки.
 //Реализации этого интерфейса выполняют фактическую работу по отображению каждой строки в объект результата
@@ -18,13 +19,51 @@ public class TaxiOrderAcceptanceRowMapper implements RowMapper<TaxiOrderAcceptan
     public TaxiOrderAcceptance mapRow(ResultSet resultSet, int i) throws SQLException {
         //Методы ResultSet.getXXX предоставляют доступ к значениям в колонках в текущей строке.
         // В пределах одной строки значения могут быть считаны в любом порядке
+
+        /*-----------------------UserVehicle-----------------------------*/
+        UserVehicle userVehicle = new UserVehicle();
+        userVehicle.setId(resultSet.getInt("uv_id"));                                     //userVehicle.setId(resultSet.getInt(1));
+        userVehicle.setStartDate(resultSet.getTimestamp("uv_start_date").toLocalDateTime());      //userVehicle.setStartDate(resultSet.getTimestamp("start_date").toLocalDateTime());
+        //userVehicle.setStartDate(resultSet.getObject(2,LocalDateTime.class));                 //userVehicle.setStartDate(resultSet.getObject(2,LocalDateTime.class));
+        if (resultSet.getTimestamp("uv_end_date") != null)
+            userVehicle.setEndDate(resultSet.getTimestamp("uv_end_date").toLocalDateTime());
+        userVehicle.setIsCurrentUserMachine(resultSet.getInt("uv_is_current_user_machine"));
+
+        /*-----------------------TaxiDispatcherOrder-----------------------------*/
+        TaxiDispatcherOrder taxiDispatcherOrder = new TaxiDispatcherOrder();
+        taxiDispatcherOrder.setId(resultSet.getInt(1));                         //taxiDispatcherOrder.setId(resultSet.getInt("tdo_id"));
+        if (resultSet.getTimestamp("dto_date_time_order") != null)
+            taxiDispatcherOrder.setDateTimeOrder(resultSet.getTimestamp("dto_date_time_order").toLocalDateTime());
+        taxiDispatcherOrder.setUserName(resultSet.getString("tdo_user_name"));
+        taxiDispatcherOrder.setUserPhone(resultSet.getString("tdo_user_phone"));
+        taxiDispatcherOrder.setAddressDeparture(resultSet.getString("tdo_address_departure"));
+        taxiDispatcherOrder.setAddressArrival(resultSet.getString("tdo_address_arrival"));
+        if (resultSet.getTimestamp("tdo_start_date") != null)
+            taxiDispatcherOrder.setStartDate(resultSet.getTimestamp("tdo_start_date").toLocalDateTime());
+        if (resultSet.getTimestamp("tdo_end_date") != null)
+            taxiDispatcherOrder.setEndDate(resultSet.getTimestamp("tdo_end_date").toLocalDateTime());
+        taxiDispatcherOrder.setFulfilled(resultSet.getInt("tdo_fulfilled"));
+
+        /*-----------------------TaxiUserOrder-----------------------------*/
+        TaxiUserOrder taxiUserOrder = new TaxiUserOrder();
+        taxiUserOrder.setId(resultSet.getInt("tuo_id"));                       //taxiUserOrder.setId(resultSet.getInt(1));
+        if(resultSet.getObject("tuo_date_time_order") != null)
+            taxiUserOrder.setDateTimeOrder(resultSet.getObject(2, LocalDateTime.class));
+        taxiUserOrder.setAddressDeparture(resultSet.getString("tuo_address_departure"));
+        taxiUserOrder.setAddressArrival(resultSet.getString("tuo_address_arrival"));
+        taxiUserOrder.setStartDate(resultSet.getObject("tuo_start_date", LocalDateTime.class));
+        if(resultSet.getTimestamp("tuo_end_date") != null)
+            taxiUserOrder.setEndDate(resultSet.getTimestamp("tuo_end_date").toLocalDateTime());
+        taxiUserOrder.setFulfilled(resultSet.getInt("tuo_fulfilled"));
+
+
         TaxiOrderAcceptance taxiOrderAcceptance = new TaxiOrderAcceptance();
-        taxiOrderAcceptance.setId(resultSet.getInt(1));                     //taxiOrderAcceptance.setId(resultSet.getInt("id"));
-        taxiOrderAcceptance.setIdUserVehicle((UserVehicle) resultSet.getObject(2));
-        taxiOrderAcceptance.setIdTaxiDispatcherOrder((TaxiDispatcherOrder) resultSet.getObject(3));
-        taxiOrderAcceptance.setIdTaxiUserOrder((TaxiUserOrder) resultSet.getObject(4));
-        taxiOrderAcceptance.setExecutionStatus(resultSet.getInt(5));
-        taxiOrderAcceptance.setAdoptionStatus(resultSet.getInt(6));
+        taxiOrderAcceptance.setId(resultSet.getInt("toa_id"));                     //taxiOrderAcceptance.setId(resultSet.getInt(1));
+        taxiOrderAcceptance.setIdUserVehicle(userVehicle);
+        taxiOrderAcceptance.setIdTaxiDispatcherOrder(taxiDispatcherOrder);
+        taxiOrderAcceptance.setIdTaxiUserOrder(taxiUserOrder);
+        taxiOrderAcceptance.setExecutionStatus(resultSet.getInt("toa_execution_status"));
+        taxiOrderAcceptance.setAdoptionStatus(resultSet.getInt("toa_adoption_status"));
 
         return taxiOrderAcceptance;
     }
